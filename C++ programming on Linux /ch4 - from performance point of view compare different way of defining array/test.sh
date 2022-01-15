edcfc6501e56f344de6a263f4f5aef50d883fd28
ian chu <a11025112@gmail.com>
2022-01-15 21:02:30 +0800

    ch4 - from performance point of view compare different way of defining array

---
+++ C++ programming on Linux /ch4 - from performance point of view compare different way of defining array/test.sh
@@ -0,0 +1,18 @@
+echo "----------int------------"
+./int
+./int
+./int
+./int
+./int
+echo "----------array------------"
+./array
+./array
+./array
+./array
+./array
+echo "----------vector------------"
+./vector
+./vector
+./vector
+./vector
+./vector
---
+++ C++ programming on Linux /ch4 - from performance point of view compare different way of defining array/vector.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <chrono>
+#include <array>
+#include <vector>
+
+using namespace std;
+
+
+
+
+
+int main(){
+    int myInt[100000];
+    std::array<int,100000> myArr;
+    std::vector<int> myVec;
+
+    srand(time(NULL));
+
+    chrono::high_resolution_clock::time_point time_start = chrono::high_resolution_clock::now();
+
+    for(int i=0;i<100000;i++){
+        myVec.push_back(rand()%100+1);
+    }
+
+    chrono::high_resolution_clock::time_point time_end = chrono::high_resolution_clock::now();
+    chrono::microseconds exec_time = chrono::duration_cast<chrono::microseconds>(time_end-time_start);
+    cout << "myInt array running time is " << exec_time.count() << " micro" <<endl;
+    return 0;
+}
